3a89065de4125fff9d3ebe37a3d19216ebe6e164
maryem <maryemby21@gmail.com>
2023-02-10 12:12:38 +0100

    i changed the operator + with += and cleaned some stuff it should work now

--- produits.cpp
+++ produits.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
-#include <typeinfo>
+
 using namespace std ;
 
 
 class article
 { 
-    public:
+    private:
     int code;
     string designation ;
     int quantity ;
@@ -38,7 +38,7 @@ class article
 
 class produit_fini : public article 
 {
-    public:
+    private:
         string marque;
         float prix;
         int nb_operations;
@@ -58,10 +58,16 @@ class produit_fini : public article
         int get_nb_operations() { return (nb_operations); }
         void set_operations(string o[10]) {
             for (int i = 0; i < 10; i++) {
+                do {
                 operations[i] = o[i];
+                }while ((operations[i].size())!=30);
             }
         }
-        string* get_operations() { return (operations );}
+        void set_next(produit_fini*p)
+        {
+            next=p;
+        }
+string* get_operations() { return (operations );}
 produit_fini(int code,string design,int quant,string marque,int prix,int nb,string * tab):article(code,design,quant)
 {
 set_marque(marque);
@@ -70,28 +76,28 @@ set_nb_operations(nb);
 set_operations(tab);
 next=NULL;
 }
-
-operator string (void)
-{
-    return(marque);
-}
 int read_data()
-{
- cout<<"Code";
-   cin>>code;
-   if (code==-1)
+{ 
+  int cod,quant;
+  string design;
+  cout<<"Code:"<<endl;
+   cin>>cod;
+   if (cod==-1)
    return(0);
-   cout<<"designation";
-   cin>>designation;
-   cout<<"quantité";
-   cin>>quantity;
-   cout<<"marque";
+   this->set_code(cod);
+   cout<<"designation:"<<endl;
+   cin>>design;
+   this->set_designation(design);
+   cout<<"quantité:"<<endl;
+   cin>>quant;
+   this->set_stock(quant);
+   cout<<"marque"<<endl;
    cin>>marque;
-   cout<<"prix";
+   cout<<"prix"<<endl;
    cin>>prix;
-   cout<<"le nombre d'operations";
+   cout<<"le nombre d'operations"<<endl;
    cin>>nb_operations;
-   cout<<"Les 10 operations necessaires a la fabrication:";
+   cout<<"Les 10 operations necessaires a la fabrication:"<<endl;
     for (int i = 0; i < 10; i++) {
                 cin>>operations[i];
             }
@@ -102,6 +108,7 @@ produit_fini* get_next()
 {
     return(next);
 }
+
 friend class ListeProduitFini;
 };
 
@@ -113,33 +120,43 @@ class ListeProduitFini {
            first=NULL;
         }
         void add_first(produit_fini* p) {
-            p->next=first;
+            p->set_next(first);
             first=p;
         }
-void creer()
+    produit_fini * get_first()
+       {
+        return(first);
+       }
+void create()
 {
-produit_fini p;
-while( p.read_data())
-add_first(&p);
+produit_fini* p=new produit_fini;
+while( p->read_data())
+add_first(p);
 }
-produit_fini * get_first()
+void view_list()
 {
-return(first);
-}
-void operator +=(ListeProduitFini l)
-{
-    produit_fini* p=new produit_fini;
-    p=l.get_first();
-     while (p)
+    produit_fini*p=first; 
+    while (p)
     {
-        this->add_first(p);
+        cout<<p->get_code()<<endl;
         p=p->next;
     }
-    delete p;
-    p=NULL;
 }
-
-/*~ListeProduitFini()
+ void operator +=(ListeProduitFini l)
+ {
+    produit_fini* p;
+    produit_fini* n;
+    p=l.get_first();
+    n=p;
+    int i=0;
+    while(p)
+    {   n=n->next;
+        this->add_first(p);
+        i++;
+        p=n;
+    }
+ }
+ ~ListeProduitFini()
 {
 produit_fini *p;
 produit_fini *save;
@@ -147,21 +164,10 @@ for(p=first;p;)
 {
 save=p;
 p=p->next;
-delete save;
 save=NULL;
+delete save;}
 }
-}*/
-void view_liste(produit_fini*);
 };
-void ListeProduitFini:: view_liste(produit_fini*first)
-{
-if (first)
-{
-cout<<first->get_code();
-cout<<"haha";
-view_liste(first->next);
-}
-}
 
 
 int main()
@@ -169,11 +175,17 @@ int main()
     string tab[10]={"1","2","2","E","Z","E","E","U","E","E"};
     produit_fini a(100,"ara",12,"1GG",12,12,tab);
     produit_fini b(200,"ara",12,"1GG",12,12,tab);
-    ListeProduitFini l;
-    l.add_first(&a);
+    produit_fini c(300,"ara",12,"1GG",12,12,tab);
+
     ListeProduitFini p;
-    p.add_first(&b);
+    ListeProduitFini l;
+    p.add_first(&a);
+    cout<<"before:"<<endl;
+    l.add_first(&b);
+    l.add_first(&c);
+    p.view_liste();
     p+=l;
-    p.view_liste(p.get_first());
+    cout<<"after:"<<endl;
+    p.view_liste();
     return(0);
 }
